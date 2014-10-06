e61c07f81b24b98a305da051c552f57f3ddec971
mattpascoe <hornet136@gmail.com>
2014-10-06 15:57:10 -0600

    Update plugin_info.php
    bumping version number

--- plugin_info.php
+++ plugin_info.php
@@ -5,7 +5,7 @@ Your plugin must have a short description and a version number
 $plug_name = basename(dirname(__FILE__));
 
 $plugin_description = 'Network device config archives';
-$plugin_version = '1.0';
+$plugin_version = '1.5';
 $plugin_help_url = 'http://opennetadmin.com/docs/plugins/'.$plug_name;
 
 
