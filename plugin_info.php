8686f10285a397960fc0e102261bc2662bf73336
Matt Pascoe <matt@opennetadmin.com>
2015-11-02 15:25:37 -0800

    update version as well as document links

--- plugin_info.php
+++ plugin_info.php
@@ -5,8 +5,8 @@ Your plugin must have a short description and a version number
 $plug_name = basename(dirname(__FILE__));
 
 $plugin_description = 'Network device config archives';
-$plugin_version = '1.5';
-$plugin_help_url = 'http://opennetadmin.com/docs/plugins/'.$plug_name;
+$plugin_version = '1.6';
+$plugin_help_url = 'https://github.com/opennetadmin/'.$plug_name.'/wiki';
 
 
 ?>
